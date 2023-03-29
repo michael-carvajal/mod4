f5b55ddd28053621e741f8a1abc59efeac11c5b5
michael-carvajal <michaelcarvajal.work@gmail.com>
2023-03-29 12:53:27 -0400

    3/29/23 pt 3

--- week10/demo/sql_stuff/example.sql
+++ week10/demo/sql_stuff/example.sql
@@ -1,1 +1,23 @@
-SELECT "Whats up my nerds"
+CREATE TABLE pokemon (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(50) NOT NULL UNIQUE,
+    height FLOAT(4,2) NOT NULL,
+    weight FLOAT(5,2) NOT NULL,
+    rareness VARCHAR(30) DEFAULT 'rare',
+    evolves BOOLEAN
+);
+CREATE TABLE trainer (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(50) NOT NULL UNIQUE,
+    team_size INTEGER,
+    gym_badges INTEGER
+    bike BOOLEAN
+);
+ CREATE TABLE dogs (
+   id INTEGER PRIMARY KEY AUTOINCREMENT
+   name VARCHAR(50)
+   age_yrs NUMERIC(3,1)
+   breed VARCHAR(100)
+   weight_lbs INTEGER
+   microchipped BOOLEAN
+   );
